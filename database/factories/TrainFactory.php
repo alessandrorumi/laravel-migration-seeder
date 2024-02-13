6375ebe055521adcd60af3e264132f5cbbeca877
alessandrorumi <rumi.alessandro21@gmail.com>
2024-02-13 16:33:39 +0100

    Optimize departure & arrival station

--- database/factories/TrainFactory.php
+++ database/factories/TrainFactory.php
@@ -18,12 +18,12 @@ class TrainFactory extends Factory
     {
         return [
             'company'=>fake() -> word(),
-            'departure_station'=>fake() -> words(2, true),
-            'arrival_station'=>fake() -> words(2, true),
+            'departure_station'=>fake() -> sentence(1),
+            'arrival_station'=>fake() -> sentence(1),
             'departure_time'=>fake() -> dateTimeBetween('-1 week', '+1 week'),
             'arrival_time'=>fake() -> dateTimeBetween('-1 week', '+1 week'),
             'train_code'=>fake() -> regexify('[A-Z]{5}[0-4]{3}'),
-            'carriages'=>fake() ->numberBetween(0, 20),
+            'carriages'=>fake() ->numberBetween(1, 20),
             'in-time'=>fake() -> boolean(),
             'cancelled'=>fake() -> boolean(),
         ];
